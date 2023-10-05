504458da323b341fe555403bf7489474ecd6f036
Famuwagun oluwatofunmi <tofunmifamu@gmail.com>
2023-10-05 11:48:23 -0700

    my fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * mail - entry point
- * Description: prints "Programming is like building a multilingual puzzle " to the standard ouput
- * return: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
