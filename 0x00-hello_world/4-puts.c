5cfdd08c46dbdff935aaec10db3659422f742921
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2023-10-05 10:03:04 -0700

    my fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * mail - entry point
+ * Description: prints "Programming is like building a multilingual puzzle " to the standard ouput
+ * return: Always 0 (success)
+ */
+
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
