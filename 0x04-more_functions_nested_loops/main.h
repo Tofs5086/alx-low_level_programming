ef247f999d14c03f41c402ee48bf488d42057445
Famuwagun oluwatofunmi <tofunmifamu@gmail.com>
2023-10-12 12:28:42 -0700

    my fifth commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+
+#endif /* MAIN_H */
