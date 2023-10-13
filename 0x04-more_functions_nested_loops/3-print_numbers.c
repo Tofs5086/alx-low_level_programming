0d88bb85e11e33893d4e090664e12019d33b06c9
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2023-10-13 08:29:22 -0700

    abeg

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,14 @@
 #include "main.h"
-#include "_putchar.c"
 
 /**
  * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
  */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
+
 void print_numbers(void)
 {
 	int i;
